20c2da977c746327af74a863f291569acf695be4
Vipul Vasistya <vsvipul068@gmail.com>
2026-09-24 11:34:04 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char [] sorted_s = str.toCharArray();
+            Arrays.sort(sorted_s);
+            String sorted_str = new String(sorted_s);
+            if(map.containsKey(sorted_str)){
+                map.get(sorted_str).add(str);
+            }
+            else{
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(sorted_str,list);
+            }
+        }
+        for(List<String> str : map.values()){
+            res.add(str);
+        }
+        return res;
+    }
+}
