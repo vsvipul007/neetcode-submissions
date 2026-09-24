5c8c52d77436ba9c629f601117c48b37fef7cfbb
Vipul Vasistya <vsvipul068@gmail.com>
2026-09-24 10:04:10 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+    //Frequency array method
+    if(s.length() != t.length()) return false;
+        int  []freq = new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]!=0) return false;
+        }
+        return true;
+    }
+}
